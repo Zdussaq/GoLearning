28af423399d61897960e36190eddb6242031b120
Dean Dussaq <Zdussaq@gmail.com>
2021-10-19 11:55:23 -0700

    Imported all exercises

--- ch1/echo1/main.go
+++ ch1/echo1/main.go
@@ -1,4 +1,10 @@
-//echo1 prints its command line arguments
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 4.
+//!+
+
+// Echo1 prints its command-line arguments.
 package main
 
 import (
@@ -8,9 +14,11 @@ import (
 
 func main() {
 	var s, sep string
-	for _, arg := range os.Args[1:] {
-		s += sep + arg
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
 }
+
+//!-
--- ch1/helloworld/main.go
+++ ch1/helloworld/main.go
@@ -1,3 +1,8 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 1.
+
 // Helloworld is our first Go program.
 //!+
 package main
@@ -7,3 +12,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, 世界")
 }
+
+//!-
